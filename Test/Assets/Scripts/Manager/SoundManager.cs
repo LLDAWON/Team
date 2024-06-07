ca48d5236a23b9109c8c756bb072f81d354ecb8d
LeeDawon <wkao0304@naver.com>
2024-06-07 19:34:08 +0900

    수정완료

---
+++ Test/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ClipInfo
+{
+    public string key;
+    public AudioClip clip;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField]
+    private List<ClipInfo> _clipInfos = new List<ClipInfo>();
+
+    [Header("3D Sound Option")]
+    [Range(0f, 1f)]
+    private float _volume = 1.0f;
+    private int _audioPoolSize = 30;
+    private int _soundDistanceMin = 0;
+    private int _soundDistanceMax = 10;
+
+    private AudioSource _audioSource;
+    private Dictionary<string, AudioClip> clips = new Dictionary<string, AudioClip>();
+
+    private List<GameObject> _audioObjects = new List<GameObject>();
+
+    private void Awake()
+    {
+        Instance = this;
+
+        foreach(ClipInfo clipInfo in _clipInfos)
+        {
+            clips.Add(clipInfo.key, clipInfo.clip);
+        }
+
+        _audioSource = GetComponent<AudioSource>();
+
+        for(int i = 0; i < _audioPoolSize; i++)
+        {
+            GameObject obj = new GameObject("Sound" + i);
+            AudioSource audioSource =  obj.AddComponent<AudioSource>();
+            audioSource.loop = false;
+            audioSource.volume = _volume;
+            audioSource.minDistance = _soundDistanceMin;
+            audioSource.maxDistance = _soundDistanceMax;
+            obj.transform.SetParent(transform);
+
+            _audioObjects.Add(obj);
+        }
+
+    }
+
+    public void Play2D(string key)
+    {
+        _audioSource.clip = clips[key];
+        _audioSource.Play();
+    }
+
+    public void Play3D(string key, Vector3 pos)
+    {
+        //액티브 꺼져있는 애 찾아서 플레이
+    }
+}
