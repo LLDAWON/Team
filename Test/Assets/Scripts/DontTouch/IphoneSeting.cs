e3f30922b4131e7dff801d673f59bbeb56729d43
Kim, Hyunbok <asd2590@naver.com>
2024-06-11 13:28:00 +0900

    적외선 효과
    1. 흑백셰이더에 컬러 곱하는 셰이더 구현 및 아이폰에 적용

--- Test/Assets/Scripts/DontTouch/IphoneSeting.cs
+++ Test/Assets/Scripts/DontTouch/IphoneSeting.cs
@@ -7,16 +7,15 @@ using UnityEngine.Rendering.Universal;
 public class IphoneSeting : MonoBehaviour
 {
     public Camera renderCamera; 
-    public RenderTexture renderTexture;
+    //public RenderTexture renderTexture;
    
 
     
     void Start()
     {
-        renderCamera.targetTexture = renderTexture;
+        //renderCamera.targetTexture = renderTexture;
         renderCamera.backgroundColor = Color.clear;
-        renderCamera.clearFlags = CameraClearFlags.SolidColor;
-      
+        renderCamera.clearFlags = CameraClearFlags.SolidColor;     
 
     }
 }
