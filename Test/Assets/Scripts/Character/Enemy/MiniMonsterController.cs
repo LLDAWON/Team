334dcdf064e4d5a1a20ac51b5ed961c60b279f8f
Midam <rkdckdgns97@gmail.com>
2024-06-12 04:49:58 +0900

    쪼꼬미들 / 자질구레한 버그수정

--- Test/Assets/Scripts/Character/Enemy/MiniMonsterController.cs
+++ Test/Assets/Scripts/Character/Enemy/MiniMonsterController.cs
@@ -4,5 +4,71 @@ using UnityEngine;
 
 public class MiniMonsterController : EnemyController
 {
-   
+    protected override void StateUpdate()
+    {
+        PlayerController playerController = _target.GetComponent<PlayerController>();
+        //플레이어의 부채꼴 탐색
+        Vector3 _inPlayerSight = transform.position - _target.transform.position;
+        _inPlayerSight.y = 0;
+
+        float _detectRange = playerController.GetCharacterData().DetectRange;
+
+        //감지범위안에 들어왔을때
+        if (_inPlayerSight.magnitude <= _detectRange)
+        {
+            float dot = Vector3.Dot(_inPlayerSight.normalized, playerController.transform.forward);
+
+            float theta = Mathf.Acos(dot);
+
+            float degree = Mathf.Rad2Deg * theta;
+
+            //부채꼴 안에 들어왔을때
+            if (degree <= _angleRange)
+            {
+                //불켰을때
+                if (playerController.GetIsLightOn() == true)
+                    SetState(5); //주금
+                else
+                    SetState(1); // 추적
+            }
+            //부채꼴안에 안들어왔을때
+            else
+                SetState(1); //추적
+        }
+        // 감지범위 밖일때도 추적해오자
+        else
+        {
+            SetState(1);
+        }
+    }
+    protected override void EnemyAiPattern()
+    {
+
+
+        switch (_enemyState)
+        {
+            case EnemyState.Trace:
+                {
+                    _animator.speed = 1.0f;
+                    _navigation.SetDestination(_target.position);
+                    _navigation.speed = _characterData.RunSpeed;
+                }
+                break;
+            case EnemyState.Attack:
+                {
+                    _navigation.speed = 0;
+                    _isAttack = true;
+                }
+
+                break;
+            case EnemyState.Die:
+                {
+                    _navigation.velocity = Vector3.zero;
+                    _navigation.speed = 0;
+                    Debug.Log("죽음");
+                    DestroyMonster();
+                }
+                break;
+        }
+    }
 }
