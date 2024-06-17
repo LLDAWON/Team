84c096099fb81426c97aca697d80fa5eec47d4ac
ohog10 <ohog96@naver.com>
2024-06-18 07:38:24 +0900

    냥

--- Test/Assets/Scripts/DontTouch/ObjectManager.cs
+++ Test/Assets/Scripts/DontTouch/ObjectManager.cs
@@ -12,17 +12,33 @@ public class ObjectManager : MonoBehaviour
    
     private float _speed = 30.0f;
     public float _maxRotationX = 45f;
-    
+    private Coroutine _bookCaseRotationCoroutine;
+
+
     public void BookCase()
     {
-        Vector3 currentRotation = _bookCase.transform.rotation.eulerAngles;
-
-        float newRotationX = Mathf.Clamp(currentRotation.x + _speed * Time.deltaTime, 0f, _maxRotationX);
-
-        _bookCase.transform.rotation = Quaternion.Euler(newRotationX, currentRotation.y, currentRotation.z);
+        if (_bookCaseRotationCoroutine == null)
+        {
+            _bookCaseRotationCoroutine = StartCoroutine(RotateBookCase()); 
+        }
     }
-   
 
+    private IEnumerator RotateBookCase()
+    {
+        while (!IsBookCaseRotate())
+        {
+            Vector3 currentRotation = _bookCase.transform.rotation.eulerAngles;
+            float newRotationX = Mathf.Clamp(currentRotation.x + _speed * Time.deltaTime, 0f, _maxRotationX);
+            _bookCase.transform.rotation = Quaternion.Euler(newRotationX, currentRotation.y, currentRotation.z);
+            yield return null; 
+        }
+        _bookCaseRotationCoroutine = null; 
+    }
+
+    private bool IsBookCaseRotate()
+    {
+        return Mathf.Approximately(_bookCase.transform.rotation.eulerAngles.x, _maxRotationX);
+    }
 
 
 }
