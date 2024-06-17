b0cff577097eee82b2039880fcbf7834739624fd
ohog10 <ohog96@naver.com>
2024-06-18 02:25:13 +0900

    3층수정

--- Test/Assets/Scripts/DontTouch/ObjectManager.cs
+++ Test/Assets/Scripts/DontTouch/ObjectManager.cs
@@ -8,26 +8,18 @@ public class ObjectManager : MonoBehaviour
     public static ObjectManager Instance
     { get { return _instance; } }
 
-
-    public GameObject celing;
     public GameObject _bookCase;
-    private Vector3 yPos;
-    private float speed = 10.0f;
-    public float maxRotationX = 45f;
-    public void Falling( )
-    {
-        
-       // celing.transform.Translate(Vector3.down * speed*Time.deltaTime );
-    }
+   
+    private float _speed = 30.0f;
+    public float _maxRotationX = 45f;
+    
     public void BookCase()
     {
-
         Vector3 currentRotation = _bookCase.transform.rotation.eulerAngles;
 
-        float newRotationX = Mathf.Clamp(currentRotation.x + speed * Time.deltaTime, 0f, maxRotationX);
+        float newRotationX = Mathf.Clamp(currentRotation.x + _speed * Time.deltaTime, 0f, _maxRotationX);
 
         _bookCase.transform.rotation = Quaternion.Euler(newRotationX, currentRotation.y, currentRotation.z);
-        
     }
    
 
