f047a792a9c85cd569843f7b9da39e8fffe7775e
LLDAWON <155229763+LLDAWON@users.noreply.github.com>
2024-06-21 15:28:02 +0900

    수정중인 미니맵

--- Test/Assets/Scripts/DontTouch/PlayerDirection.cs
+++ Test/Assets/Scripts/DontTouch/PlayerDirection.cs
@@ -8,25 +8,36 @@ public class PlayerDirection : MonoBehaviour
     public RectTransform directionIconRectTransform;
     public Camera miniMapCamera; 
     private Transform playerTransform; 
-    private RectTransform playerIconRectTransform; 
+    private RectTransform playerIconRectTransform;
+
+    [SerializeField]
+    private Vector3 _offset;
 
     void Start()
     {
         playerIconRectTransform = GetComponent<RectTransform>();
         playerTransform = GameManager.Instance.GetPlayer().transform;
+
+        Vector3 miniMapPos = miniMapCamera.WorldToViewportPoint(playerTransform.position);
+
+        playerIconRectTransform.localPosition = miniMapPos + _offset;
     }
 
     void Update()
     {
-        Vector3 miniMapPos = miniMapCamera.WorldToViewportPoint(playerTransform.position);
+        //Vector3 miniMapPos = miniMapCamera.WorldToViewportPoint(playerTransform.position);
 
-        miniMapPos.x *= miniMapRectTransform.rect.width;
-        miniMapPos.y *= miniMapRectTransform.rect.height;
+        /*        miniMapPos.x *= miniMapRectTransform.rect.width;
+                miniMapPos.y *= miniMapRectTransform.rect.height;*/
 
-        miniMapPos.x -= miniMapRectTransform.rect.width * 0.5f;
-        miniMapPos.y -= miniMapRectTransform.rect.height * 0.5f;
+        Vector2 worldPos = new Vector2(transform.position.x, transform.position.z) * 4;
+        //Vector2 offset = ;
+        //
+        
 
-        playerIconRectTransform.localPosition = miniMapPos;
+
+
+        //playerIconRectTransform.localPosition = miniMapPos + _offset;
 
         if (directionIconRectTransform != null)
         {
